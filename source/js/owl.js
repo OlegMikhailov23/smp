0592aade5b9614de7ebc7002e880345be7f08fdb
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-03-23 13:15:46 +0300

    start

---
+++ source/js/owl.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+$('.intro-slider').owlCarousel({
+    loop:true,
+    dots:true,
+    items:1,
+    autoplay: true,
+    nav: false,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 10000,
+    mouseDrag: true,
+    touchDrag: true,
+    margin: 0
+});
+
+});
