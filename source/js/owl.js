e1df357726bd4175ad8e7bf020c9f6d516ccf991
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-03-25 23:40:46 +0300

    creating nav

--- source/js/owl.js
+++ source/js/owl.js
@@ -4,10 +4,10 @@ $('.intro-slider').owlCarousel({
     loop:true,
     dots:true,
     items:1,
-    autoplay: true,
+    autoplay: false,
     nav: false,
     autoplaySpeed: 1000,
-    autoplayTimeout: 10000,
+    autoplayTimeout: 15000,
     mouseDrag: true,
     touchDrag: true,
     margin: 0
